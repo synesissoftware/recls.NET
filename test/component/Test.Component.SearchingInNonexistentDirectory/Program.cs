93437f11f07b14e19b31902fec878a34a545f7e1
Matt Wilson <matthew@synesis.com.au>
2017-09-11 12:37:46 +1000

    + added Test.Component.SearchingInNonexistentDirectory

---
+++ test/component/Test.Component.SearchingInNonexistentDirectory/Program.cs
@@ -0,0 +1,39 @@
+﻿
+namespace Test.Component.SearchingInNonexistentDirectory
+{
+    using global::Recls;
+
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.IO;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            try
+            {
+                string tempDir = Path.GetTempPath();
+
+                Guid guid = Guid.NewGuid();
+
+                string directory = Path.Combine(tempDir, guid.ToString());
+
+                foreach (IEntry entry in FileSearcher.Search(directory, FileSearcher.WildcardsAll))
+                {
+                }
+
+                return 0;
+            }
+            catch (Exception x)
+            {
+                Console.Error.WriteLine("exception ({0}): {1}", x.GetType().FullName, x.Message);
+
+                return 1;
+            }
+        }
+    }
+}
