81ec038fb771fe2a7f4b3ec33d29a0c5ee61d396
Matt Wilson <matthew@synesis.com.au>
2019-04-05 17:54:10 +1100

    ~ final bits

---
+++ test/scratch/test.scratch.Stat/Program.cs
@@ -0,0 +1,49 @@
+﻿
+namespace ConsoleApplication1
+{
+    using global::Recls;
+    using Recls = global::Recls.Api;
+
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                IEntry      e;
+                IEntry2_1   e2;
+
+                e   =   Recls.Stat("Z:/does-not-exist.ext");
+
+                e2 = (IEntry2_1)Recls.Stat("Z:/does-not-exist.ext", SearchOptions.StatInfoForNonexistentPath);
+
+                e2 = (IEntry2_1)Recls.Stat("Z:/does-not-exist.ext", SearchOptions.StatInfoForNonexistentPath | SearchOptions.Files);
+
+                Console.WriteLine("'{0}' is a {1} and does {2}exist", e2, e2.IsFile ? "file" : "directory", e2.Existed ? "" : "not ");
+
+                e2 = (IEntry2_1)Recls.Stat("Z:/does-not-exist/file.ext", SearchOptions.StatInfoForNonexistentPath | SearchOptions.Files);
+
+                Console.WriteLine("'{0}' is a {1} and does {2}exist", e2, e2.IsFile ? "file" : "directory", e2.Existed ? "" : "not ");
+
+                e2 = (IEntry2_1)Recls.Stat("Z:/does-not-exist/file.ext", SearchOptions.StatInfoForNonexistentPath | SearchOptions.Directories);
+
+                Console.WriteLine("'{0}' is a {1} and does {2}exist", e2, e2.IsFile ? "file" : "directory", e2.Existed ? "" : "not ");
+
+                e2 = (IEntry2_1)Recls.Stat("Z:/does-not-exist/file.ext", SearchOptions.StatInfoForNonexistentPath | SearchOptions.Directories | SearchOptions.Files);
+
+
+
+                e2 = (IEntry2_1)Recls.Stat("J:/does-not-exist.ext", SearchOptions.StatInfoForNonexistentPath | SearchOptions.Files);
+            }
+            catch(Exception x)
+            {
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
---
+++ test/scratch/test.scratch.Stat/Properties/AssemblyInfo.cs
@@ -0,0 +1,24 @@
+﻿
+// Updated: 25th May 2018
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Test.Scratch.Stat")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Synesis Software Pty Ltd")]
+[assembly: AssemblyProduct("recls.NET")]
+[assembly: AssemblyCopyright("Copyright © Synesis Software Pty Ltd 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("2.0.1100.0")]
+
+/* ///////////////////////////// end of file //////////////////////////// */
+
